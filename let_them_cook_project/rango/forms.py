034614d081a036c8c77db4185be846dcd5df85d5
Camilla Cummins <2796631C@student.gla.ac.uk>
2024-03-15 15:59:37 +0000

    no userprofile model

--- let_them_cook_project/rango/forms.py
+++ let_them_cook_project/rango/forms.py
@@ -34,7 +34,3 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'email', 'password',)
 
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ('website', 'picture',)
